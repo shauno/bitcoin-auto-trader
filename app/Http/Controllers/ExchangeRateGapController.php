cf3dae0e9f53643c8f050b91f3648aa6fd48feeb
Shaun Alberts <shaunalberts@gmail.com>
2017-12-20 09:34:27 +0200

    Change rolling average gap to 12 hours

--- app/Http/Controllers/ExchangeRateGapController.php
+++ app/Http/Controllers/ExchangeRateGapController.php
@@ -18,7 +18,7 @@ class ExchangeRateGapController
 
     public function usdZarRollingAverage(ExchangeRateReporter $exchangeRateReporter)
     {
-         $list = $exchangeRateReporter->getExchangeRateGap(strtotime('-6 hours'), time());
+         $list = $exchangeRateReporter->getExchangeRateGap(strtotime('-12 hours'), time());
 
          $sum = 0;
          foreach ($list as $rate) {
